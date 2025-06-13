7442a09faf4643bf4faa0b4d809a376b57c46759
Eishan Ashraf <ashrafeishan@gmail.com>
2025-06-13 18:04:41 -0400

    WIP

--- clubhub-web/src/app/clubSearch/page.tsx
+++ clubhub-web/src/app/clubSearch/page.tsx
@@ -177,7 +177,7 @@ export default function clubSearchPage() {
               clubSearch();
             }}
             className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
-          >Search for Posts 
+          >Search for Clubs 
           </button>
         </div>
       </div>
