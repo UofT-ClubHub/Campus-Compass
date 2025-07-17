013a8ce4054feeb24c17e1af5242b5c0de058720
Eishan Ashraf <ashrafeishan@gmail.com>
2025-07-17 01:16:36 -0400

    created unit test setup to merge into other branches

---
+++ clubhub-web/jest.config.ts
@@ -0,0 +1,17 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(test|spec).[jt]s?(x)'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+  transform: {
+    '^.+\\.(ts|tsx)$': ['babel-jest', { configFile: './babel-jest.config.js' }],
+  },
+};
+
+export default config;
